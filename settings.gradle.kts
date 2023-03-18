6af3029f1e2038fc0d1685e40ab7b8ede20c43a7
Arumugam J <sixface@msn.com>
2023-03-18 18:24:10 -0400

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "vid-con"
+
---
+++ src/jvmMain/kotlin/Server.kt
@@ -0,0 +1,21 @@
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    embeddedServer(Netty, 9090) {
+        routing {
+            get("/hello") {
+                call.respondText("Hello, API!")
+            }
+        }
+    }.start(wait = true)
+}
